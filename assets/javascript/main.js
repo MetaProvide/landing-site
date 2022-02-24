996b2ecbfe5350e7b08fa90453bdabc58abcbaef
Henry Bergström <henrybergstrom@protonmail.com>
2022-02-24 14:30:55 +0000

    added js validation for newsletter

--- assets/javascript/main.js
+++ assets/javascript/main.js
@@ -1,18 +1,40 @@
+// Util functions
+
 function isVisible(element) {
   return element.offsetWidth > 0 && element.offsetHeight > 0;
 }
+
+ 
+function validateEmail(email) {
+      var re = /\S+@\S+\.\S+/;
+      return re.test(email);
+}
+
+
 
 function main(_) {
   // Trigger hamburger menu open/close
   const navListCheckBoxEl = document.querySelector("nav .checkbox");
   const navListEl = document.querySelector(".nav-list");
   const hamburgerEl = document.querySelector(".hamburger-lines");
+  const formEl = document.querySelector('#sib-form');
+  const submitButtonEl = document.querySelector('#submit-button');
 
   if (isVisible(hamburgerEl) && navListEl) {
     navListCheckBoxEl.addEventListener("click", (_) => {
       navListEl.classList.toggle("open");
     });
   }
+
+  if (formEl) {
+    formEl.addEventListener('input', (_) => {
+      const isAgreedToTerms = formEl.elements["OPT_IN"].checked
+      const isValidEmail = validateEmail(formEl.elements["EMAIL"].value);
+
+      submitButtonEl.disabled = !(isAgreedToTerms && isValidEmail)
+    });
+  }
+
 }
 
 window.addEventListener("DOMContentLoaded", main);
