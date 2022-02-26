85c2cfb1f09d90fb0bcc4ec2c4f96adb4f6dde46
Henry Bergström <henrybergstrom@protonmail.com>
2022-02-26 19:47:37 +0000

    fix form validation on client side

--- assets/javascript/main.js
+++ assets/javascript/main.js
@@ -4,21 +4,18 @@ function isVisible(element) {
   return element.offsetWidth > 0 && element.offsetHeight > 0;
 }
 
- 
 function validateEmail(email) {
-      var re = /\S+@\S+\.\S+/;
-      return re.test(email);
+  var re = /\S+@\S+\.\S+/;
+  return re.test(email);
 }
-
-
 
 function main(_) {
   // Trigger hamburger menu open/close
   const navListCheckBoxEl = document.querySelector("nav .checkbox");
   const navListEl = document.querySelector(".nav-list");
   const hamburgerEl = document.querySelector(".hamburger-lines");
-  const newLetterformEl = document.querySelector('#sib-form');
-  const submitButtonEl = document.querySelector('#submit-button');
+  const newsletterFormEl = document.querySelector("#sib-form");
+  const submitButtonEl = document.querySelector("#submit-button");
 
   if (isVisible(hamburgerEl) && navListEl) {
     navListCheckBoxEl.addEventListener("click", (_) => {
@@ -27,15 +24,16 @@ function main(_) {
   }
 
   // Validates input on newsletter subscription
-  if (newLetterformEl) {
-    newLetterformEl.addEventListener('input', (_) => {
-      const isAgreedToTerms = formEl.elements["OPT_IN"].checked
-      const isValidEmail = validateEmail(formEl.elements["EMAIL"].value);
+  if (newsletterFormEl) {
+    newsletterFormEl.addEventListener("input", (_) => {
+      const isAgreedToTerms = newsletterFormEl.elements["OPT_IN"].checked;
+      const isValidEmail = validateEmail(
+        newsletterFormEl.elements["EMAIL"].value
+      );
 
-      submitButtonEl.disabled = !(isAgreedToTerms && isValidEmail)
+      submitButtonEl.disabled = !(isAgreedToTerms && isValidEmail);
     });
   }
-
 }
 
 window.addEventListener("DOMContentLoaded", main);
