8877727bc64d41002ee908a318fb74b1bff2f2d1
frozenbanana <henrybergstrom@protonmail.com>
2022-03-23 10:10:49 +0100

    form submit on js side

--- assets/javascript/main.js
+++ assets/javascript/main.js
@@ -27,6 +27,63 @@ const blacklistedTextRegexes = [
 	/^.*[\u0400-\u04FF].*$/  // russian characters
 ];
 
+function sendInBluePost(url) {
+  const successMessageWrapperEl = document.querySelector('#success-message');
+  const errorMessageWrapperEl = document.querySelector('#error-message');
+
+  successMessageWrapperEl.style.display = 'none';
+  errorMessageWrapperEl.style.display = 'none';
+  return fetch(
+    url,
+    {method: 'POST'})
+    .then(resp => successMessageWrapperEl.style.display = 'block' )
+    .catch(err => errorMessageWrapperEl.style.display = 'block' )
+}
+
+function handleContactFormSubmit(ev) {
+  ev.preventDefault();
+  const errorMessageWrapperEl = document.querySelector('#error-message');
+  const errorMessageEl        = document.querySelector('#error-message .sib-form-message-panel__inner-text');
+  const isValidEmail = validateEmail(ev.target.elements["EMAIL"].value);
+  const isAcceptedEmail = !isBlacklisted(ev.target.elements["EMAIL"].value, blacklistedEmailRegExes);
+  const isAcceptedMessage = !isBlacklisted(ev.target.elements["MESSAGE"].value, blacklistedTextRegexes);
+  let customMessages = [];
+  if (!isValidEmail) customMessages.push("Invalid email");
+  if (!isAcceptedEmail) customMessages.push("Unacceptable email");
+  if (!isAcceptedMessage) customMessages.push("Unaccptable message");
+  if (!isValidEmail || !isAcceptedEmail || !isAcceptedMessage) {
+    errorMessageEl.innerText = "Your message cannot be saved. "+ customMessage.join(', ');
+    errorMessageWrapperEl.style.display = 'block';
+    return;
+  }
+  
+  sendInBluePost(
+    "https://c357f7a2.sibforms.com/serve/MUIEAHshO2aQDmOk6-QyQcLnWoFoApB8_qwUDh5tToa_qX4ojBxrrl97k4eEVrTr7ar4RFFW1UeeffuKh4YZ0ak_xXO59w6TpGNylNZicUbCtj4EuaqZmJeTB-t_i_1mRETsdgoQcFyqEdzhCzft3KHRFwnsKf6ttjKh8njHHnjMZZcRk6NvgNZsmWQ92HIVNZOC9RKdtNiLSBkj"
+  );
+}
+
+function handleNewsletterSubmit(ev) {
+  ev.preventDefault();
+  const errorMessageWrapperEl = document.querySelector('#error-message');
+  const errorMessageEl        = document.querySelector('#error-message .sib-form-message-panel__inner-text');
+  const isValidEmail = validateEmail(ev.target.elements["EMAIL"].value);
+  const isAcceptedEmail = !isBlacklisted(ev.target.elements["EMAIL"].value, blacklistedEmailRegExes);
+  const isAgreedToTerms = ev.target.elements["OPT_IN"].checked;
+  let customMessages = [];
+  if (!isValidEmail) customMessages.push("Invalid email");
+  if (!isAcceptedEmail) customMessages.push("Unacceptable email");
+  if (!isAgreedToTerms) customMessages.push("Not agreed to terms");
+  if (!isValidEmail || !isAcceptedEmail || !isAgreedToTerms) {
+    errorMessageEl.innerText = "Your subscription cannot be saved. "+ customMessage.join(', ');
+    errorMessageWrapperEl.style.display = 'block';
+    return;
+  }
+
+  sendInBluePost(
+    "https://c357f7a2.sibforms.com/serve/MUIEAKdLkyBf-gtNAPxrCwZYOujTDaz7WDt0PWmjoelUdASenwQhKDzHFp4X6Onqk7F3u985sk0AUpjedfa7W1QoXGq03uQOM5BVMn7rOnxPkhuvszEdY-I3_qKZu1j43cc4lnJgK_Csl7flGU9qdaqHDh-a3vuI1Fpfg0y1JbJoW-btLG2wQfFqAcFJqqeNuWTfknRFgtfRJVJZ"
+  );
+}
+
 function main(_) {
   const navListCheckBoxEl = document.querySelector("nav .checkbox");
   const navListEl = document.querySelector(".nav-list");
@@ -35,6 +92,7 @@ function main(_) {
   const contactFormEl = document.querySelector("#contact-section form");
   const submitButtonEl = document.querySelector("#submit-button");
   
+  console.log(contactFormEl);
   // Trigger hamburger menu open/close
   if (isVisible(hamburgerEl) && navListEl) {
     navListCheckBoxEl.addEventListener("click", (_) => {
@@ -44,6 +102,8 @@ function main(_) {
 
   // Validates input on newsletter subscription
   if (newsletterFormEl) {
+    newsletterFormEl.addEventListener("submit", handleNewsletterSubmit);
+
     newsletterFormEl.addEventListener("input", (_) => {
       const isAgreedToTerms = newsletterFormEl.elements["OPT_IN"].checked;
       const isValidEmail = validateEmail(
@@ -60,6 +120,7 @@ function main(_) {
 
   // Validates input on contact form
   if (contactFormEl) {
+    contactFormEl.addEventListener("submit", handleContactFormSubmit);
     contactFormEl.addEventListener("input", (_) => {
       const isValidEmail = validateEmail(contactFormEl.elements["EMAIL"].value);
       const isAcceptedEmail = !isBlacklisted(contactFormEl.elements["EMAIL"].value, blacklistedEmailRegExes);
